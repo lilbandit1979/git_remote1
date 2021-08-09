e083f0c1698222cb0544f1115aef311502e51de8
steve <steve@localhost.localdomain>
2021-08-09 18:26:28 +0100

    More loops practice

--- loops.py
+++ loops.py
@@ -5,9 +5,17 @@ for i in range(11):
 for i in range(11):
     print('* +', i)
 
-
+'''
 user_num = int(input('Pick a number for times tables'))
 
 for i in range(13):
     print(user_num, '*', i, '=', user_num*i)
+'''
+for i in range(1,11):
+    print('*', end = ' ')
 
+name = 'Stephen'
+counter = 0
+for letter in name:
+    print(letter, 'comes in ', counter)
+    counter +=1
