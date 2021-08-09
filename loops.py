142900ac7645e55740b5671f190acfcc4df61587
steve <steve@localhost.localdomain>
2021-08-09 18:17:28 +0100

    Loops practice

---
+++ loops.py
@@ -0,0 +1,13 @@
+#Loops practice
+for i in range(11):
+    print (i * 1)
+
+for i in range(11):
+    print('* +', i)
+
+
+user_num = int(input('Pick a number for times tables'))
+
+for i in range(13):
+    print(user_num, '*', i, '=', user_num*i)
+
